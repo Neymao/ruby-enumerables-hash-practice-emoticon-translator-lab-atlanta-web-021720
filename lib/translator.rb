5a734392e9913a52e0251bf238f2bf89818abc8c
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-29 21:51:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,16 @@ end
 def get_japanese_emoticon
   # code goes here
   
-  
+  #load_library('./lib/emoticons.yml')
+
+def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
+  library = load_library(file_path)
+  if library["get_emoticon"].include?(english_emoticon) 
+    library["get_emoticon"][english_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
+end
   
   
   
@@ -34,4 +43,9 @@ end
 
 def get_english_meaning
   # code goes here
+  
+  
+  
+  
+  
 end
